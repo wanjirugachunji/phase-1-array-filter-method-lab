a7bb561bcd0c5d8d186c7597dc747d6a6afcd647
penina wanjiru <penina.wanjiru@student.moringaschool.com>
2022-06-11 15:23:23 +0300

    commit tree 8e6d9e889f3e609cb92cdb39e405aef4c26b1c67

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(names, searchName){
+    return names.filter(driver => searchName.toLowerCase() === driver.toLowerCase())
+  }
+
+  function fuzzyMatch(names, searchStr){
+    return names.filter(driver => driver.startsWith(searchStr))
+  }
+
+  function matchName(names, nameStr){
+    return names.filter(driver => driver.name === nameStr)
+  } 
